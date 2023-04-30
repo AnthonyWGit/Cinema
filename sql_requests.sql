f1360869c05982b7c753fc82cb48ae683c60788c
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-30 16:54:54 +0200

    requests sorting film by duration

--- sql_requests.sql
+++ sql_requests.sql
@@ -3,3 +3,9 @@
 SELECT film.titre_film, DATE_FORMAT(SEC_TO_TIME(film.duree_film * 60), "%H:%i") AS "Durée", film.dateSortie_film, personne.nom, personne.prenom FROM film 
 INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
 INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+
+--Films exceeing 2 hours and 15 minuts
+
+SELECT film.titre_film, DATE_FORMAT(SEC_TO_TIME(film.duree_film * 60), "%H:%i") AS "Durée" FROM film 
+WHERE film.duree_film > 135
+ORDER BY film.duree_film DESC
