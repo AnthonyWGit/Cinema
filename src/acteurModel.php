d521249462f9485c6144931ccfd60d5f75bb8312
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-09 11:17:38 +0200

    Split the big model into smaller named ones

---
+++ src/acteurModel.php
@@ -0,0 +1,26 @@
+<?php
+function connexion()
+{
+    try {
+        $mySQLconnection = new PDO(                                                     //Connecting to SQL server
+            'mysql:host=127.0.0.1;dbname=landing_page;charset=utf8',
+            'root',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+        return $mySQLconnection;
+    } catch (\Exception $e) 
+    { 
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+function getActeurs() 
+{
+    $mySQLconnection = connexion();
+    $sqlQuery = 'SELECT * FROM acteur INNER JOIN personne ON acteur.id_personne = personne.id_personne'; //priceF means priceFormated
+    $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+    $stmt->execute();                                                     //The data we retrieve are in array form
+    $acteurs = $stmt->fetchAll();
+    return $acteurs;
+}
---
+++ src/genreModel.php
@@ -0,0 +1,26 @@
+<?php
+function connexion()
+{
+    try {
+        $mySQLconnection = new PDO(                                                     //Connecting to SQL server
+            'mysql:host=127.0.0.1;dbname=landing_page;charset=utf8',
+            'root',
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+        return $mySQLconnection;
+    } catch (\Exception $e) 
+    { 
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+function getGenres() 
+{
+    $mySQLconnection = connexion();
+    $sqlQuery = 'SELECT * FROM genre'; //priceF means priceFormated
+    $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+    $stmt->execute();                                                     //The data we retrieve are in array form
+    $genres = $stmt->fetchAll();
+    return $genres;
+}
