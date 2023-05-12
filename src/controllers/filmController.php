6b0a46ea210d4c710dd8392e74a0210c3689e216
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-12 09:49:31 +0200

    basic filters sanitize ints and texts

--- src/controllers/filmController.php
+++ src/controllers/filmController.php
@@ -9,5 +9,30 @@ function displayFilms()
 
 function updateFilms($filmData, $idZ)
 {
-    updateFilmsModel($filmData, $idZ);
+    if (isset($filmData) && !empty(array_filter($filmData)))        //Checking if the user put something in the form. Using array_filter
+    {                                                               //because $filmData is an array
+        foreach ($filmData as $fieldName => $value)
+        {                                       //Need the following switch case to the filter different dataTypes
+            switch ($fieldName)
+            {
+                case "dateSortie_film":
+                    $filteredValue = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+                    $filmData[$fieldName] = $filteredValue;
+                    echo("Filtered dateSortie");
+                    break;
+                    
+                default:
+                    $filteredValue = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $filmData[$fieldName] = $filteredValue;
+                    echo ("Filtered named fields");
+                    break;                 
+            }
+        }
+        updateFilmsModel($filmData, $idZ);        
+    }
+    else
+    {
+
+    }
+
 }
