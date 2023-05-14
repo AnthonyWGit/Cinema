5d971453b27c1f27d789c203ec2c2c999c794f12
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-14 13:48:09 +0200

    controller for synopsis filters needs to be done

--- src/controllers/synopsisController.php
+++ src/controllers/synopsisController.php
@@ -1,7 +1,18 @@
 <?php 
+require_once ("src/models/filmModel.php");
 
-function displaySynopsis()
+function displaySynopsis($id)
 {
+    $synopsisIsEmpty = true;
+    $film = getFilms(); // we don't actually display all the films but we need it to get $films["synopsis"];
+    (empty($film[$id]["synopsis"])) ? $synopsisIsEmpty = true : $synopsisIsEmpty = false;
     require("views/templates/editSynopsis.php");
+}
+
+function editSynopsis($textSynopsis, $id)
+{
+
+    updateFilmSynopsis($textSynopsis, $id);
+    header("Location:index.php?action=displayFilms");
 }
 
