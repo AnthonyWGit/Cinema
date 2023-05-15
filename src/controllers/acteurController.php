ea30d1bc3f6ba0b2831a0bec822393b9d129e25a
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-15 16:26:03 +0200

    controller calling function from model no filters

--- src/controllers/acteurController.php
+++ src/controllers/acteurController.php
@@ -1,7 +1,13 @@
 <?php 
 require_once ("src/models/acteurModel.php");
+
 function displayActeurs()
 {
     $acteurs = getActeurs();
     require "views/templates/acteursListing.php";
+}
+
+function updateActeur($dataActeurs,$id)
+{
+    updateActeurModel($dataActeurs,$id);
 }
