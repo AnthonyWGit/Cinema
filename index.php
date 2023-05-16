2f0688c0abc7b25b220ced7257beb18a50033915
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-16 10:33:52 +0200

    adding comments to have a better readability

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once("src/controllers/acteurController.php");
 require_once("src/controllers/filmController.php");
 require_once("src/controllers/synopsisController.php");
 
+//---------------------- ACTIONS UPDATE---------------------------------------
+
 if (isset($_GET["action"]) &&  $_GET["action"] == "updateFilms")
 {   
     $dataFilm = $_POST;
@@ -18,6 +20,11 @@ if (isset($_GET["action"]) &&  $_GET["action"] == "updateActeur")
     var_dump($_GET);
     updateActeur($dataActeurs, $id);
 }
+
+//--------------------END ACTIONS UPDATE ---------------------------------------
+
+//----------------------ACTIONS DISPLAY -----------------------------------------
+
 else if (isset($_GET["action"]) && $_GET["action"] == "displayFilms")
 {
     displayFilms();
@@ -26,26 +33,10 @@ else if (isset($_GET["action"]) && $_GET["action"] == "displayActeurs")
 {
     displayActeurs();
 }
-else if (isset($_GET["action"]) && $_GET["action"] == "uploadFile")
-{
-    $id = $_GET["id_film"];
-    $file = $_FILES;
-    var_dump($_FILES);
-    uploadFile($file,$id);
-}
 
-else if (isset($_GET["action"]) && $_GET["action"] == "goToSynopsis")
-{
-    $id = $_GET["id"];
-    displaySynopsis($id);
-}
+//------------------END ACTION DISPLAY-----------------------------------------------
 
-else if (isset($_GET["action"]) && $_GET["action"] == "editSynopsis")
-{
-    $id = $_GET["id"];
-    $textSynopsis = $_POST["textSynopsis"];
-    editSynopsis($textSynopsis, $id);
-}
+//-------------------------ACTIONS ADD STUFF ----------------------------------
 
 else if (isset($_GET["action"]) && $_GET["action"] == "addFilm")
 {
@@ -62,12 +53,47 @@ else if (isset($_GET["action"]) && $_GET["action"] == "addActeur")
     addActeur($actorData);
 }
 
+//----------------------------------END ACTIONS ADD STUFF ----------------------------
+
+//------------------------------ACTION DELETE STUFF-------------------------------------
 
 else if (isset($_GET["action"]) && $_GET["action"] == "deleteFilm")
 {
     $id = $_GET["id"];
     deleteFilm($id);
 }
+
+// ----------------------------END ACTION DELETE STUFF----------------------------------------
+
+//----------------------------ACTION UPLOAD FILE-----------------------------------------
+
+else if (isset($_GET["action"]) && $_GET["action"] == "uploadFile")
+{
+    $id = $_GET["id_film"];
+    $file = $_FILES;
+    var_dump($_FILES);
+    uploadFile($file,$id);
+}
+
+//-----------------------------------END ACTION UPLOAD FILE ------------------------------------
+
+//-------------------------------MISC--------------------------------------------------
+
+else if (isset($_GET["action"]) && $_GET["action"] == "goToSynopsis")
+{
+    $id = $_GET["id"];
+    displaySynopsis($id);
+}
+
+else if (isset($_GET["action"]) && $_GET["action"] == "editSynopsis")
+{
+    $id = $_GET["id"];
+    $textSynopsis = $_POST["textSynopsis"];
+    editSynopsis($textSynopsis, $id);
+}
+
+// ---------------------------------DEFAULT--------------------------------
+
 else //page when landing on site 
 {
     displayFilms();
