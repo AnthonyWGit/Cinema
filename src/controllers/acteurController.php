99f88af38d5dd9a5e2a880d1f46fabb3617d0e92
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-16 15:41:23 +0200

    more readable. addActeur checks actually complete

--- src/controllers/acteurController.php
+++ src/controllers/acteurController.php
@@ -1,12 +1,10 @@
 <?php 
 require_once ("src/models/acteurModel.php");
-
 function displayActeurs()
 {
     $acteurs = getActeurs();
     require "views/templates/acteursListing.php";
 }
-
 function updateActeur($dataActeurs,$id)
 {
     $authorizedSexStrings = ["h","f","a","homme","femme","autre"];      //We want the user to only put that
@@ -35,18 +33,19 @@ function updateActeur($dataActeurs,$id)
                     echo "test not ok";
                     break;
                 }
-
             case $fieldname == "sexe";
                 if (in_array(mb_strtolower($filteredDataActeurs),$authorizedSexStrings)) //We get rid of case sensivity problem
                 {
                     echo "test ok";
+                    $filteredDataActeurs = ucfirst(strtolower($filteredDataActeurs));     //Il like having
+                    $permission = true;                                     //value displayed as UPPERCASElowercase
                     $filteredDataActeurs = ucfirst(strtolower($filteredDataActeurs));     //value displayed as UPPERCASElowercase-- I like having the final output as "Homme"
                     if (array_key_exists($filteredDataActeurs, $transformation))          //"Femme" or "Autre"  
                     $filteredDataActeurs = $transformation[$filteredDataActeurs];         //So i check if the the input is "h" "f" or "a" and transform
                     $permission = true;
                     break;
                 }
-                
+
                 else
                 {
                     echo "test not ok";
@@ -58,14 +57,26 @@ function updateActeur($dataActeurs,$id)
                 break;
         }
     }
-    $permission == true ? updateActeurModel($filteredDataActeurs,$id,$fieldname) : null ;
-    header("Location:index.php?action=displayActeurs");
-//We need to keep in mind we are working with a filtered value  comming from a string
+    if($permission)
+    {
+        updateActeurModel($filteredDataActeurs,$id,$fieldname);//We need to keep in mind we are working with a filtered value  comming from a string
                                                             //3 args so we retain the fieldname
+        header("Location:index.php?action=displayActeurs");        
+    }
+    else
+    {
+        echo "Error";
+    }
 }
+
+
 
 function addActeur($acteurData)
 {
+
+    //------------- INITIALIZATION VAR--------------------------------
+    $authorizedSexStrings = ["h","f","a","homme","femme","autre"];      //We want the user to only put that
+    $transformation = ["H" => "Homme", "F" => "Femme", "A" => "Autre"]; //If user put a correct input like "h" "f" or "a"
     $permission = true;                                //Another method of filtering using filter_var_array instead of
     $filters = [                                        //foreaching through all of the field and adding filtered values to a new array
         "nom" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,    //like an idiot 
@@ -73,13 +84,26 @@ function addActeur($acteurData)
         "dateDeNaissance" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
         "sexe" => FILTER_SANITIZE_FULL_SPECIAL_CHARS
     ];
+    //-----------------SANITIZING-------------------------------------
     $filteredActeurData = filter_var_array($acteurData,$filters);
+    $sexeInput = mb_strtolower($filteredActeurData["sexe"]);       
+    //-----------------END INTIALIZATION VAR--------------------------    
+    //-----------------CHEKING DATE FORMAT
     $dateObj = DateTime::createFromFormat('Y-m-d', $filteredActeurData["dateDeNaissance"]);
-    if (!$dateObj) $permission = false;          //If the creation of object DateTime is false it means the user put wrong format
-    $authorizedSexStrings = ["h","f","a","homme","femme","autre"];      //We want the user to only put that
-    if (in_array(mb_strtolower($filteredActeurData),$authorizedSexStrings)) //We get rid of case sensivity problem
-    
+    if (!$dateObj) $permission = false;          //If the creation of object DateTime is false it means the user put wrong format    
+    //-----------------CHECKING SEX------------------------
 
+    if (in_array($sexeInput,$authorizedSexStrings)) //We get rid of case sensivity problem
+    {
+        echo "test ok-------". var_dump($filteredActeurData["sexe"]);
+        $sexeInput = ucfirst($sexeInput);     //value displayed as UPPERCASElowercase-- I like having the final output as "Homme"
+        if (array_key_exists($sexeInput, $transformation))          //"Femme" or "Autre"  
+        {
+            $filteredActeurData["sexe"] = $transformation[$sexeInput];         //So i check if the the input is "h" "f" or "a" and transform
+            $permission = true;            
+        }
+    }
+    //-------------------------ACTION ---------------------------
     if ($permission){
         addActeurModel($filteredActeurData);
     }
@@ -87,7 +111,6 @@ function addActeur($acteurData)
     echo "Error";
     }
 }
-
 function deleteActeur($id)
 {
     deleteActeurModel($id);
