fe177cdd92b5c8ccdfb62b8c5ab15b61145c06c3
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-21 20:18:51 +0200

    Added film placeholder for poster

--- src/controllers/afficheController.php
+++ src/controllers/afficheController.php
@@ -5,5 +5,6 @@ function displayAffiche($id)
 {
     $pathFile = getPathfile($id); //Getting the filepath from DB
     $synopsis = getSynopsis($id);
+    if (in_array(!empty($pathFile[0]["image_film"]),$pathFile)) $thereIsAFile = true; else $thereIsAFile = false;
     require_once("views/templates/affiche.php");
 }
