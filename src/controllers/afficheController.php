b1b03d7077cf16ea0fc608a10e5ed90216125326
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-22 09:50:20 +0200

    controller and model for affiche retrieving data

--- src/controllers/afficheController.php
+++ src/controllers/afficheController.php
@@ -1,8 +1,10 @@
 <?php
 require_once ("src/models/afficheModel.php");
+require_once ("src/models/filmModel.php");
 
 function displayAffiche($id) 
 {
+    $filmData = getOneFilmData($id);
     $pathFile = getPathfile($id); //Getting the filepath from DB
     $synopsis = getSynopsis($id);
     if (in_array(!empty($pathFile[0]["image_film"]),$pathFile)) $thereIsAFile = true; else $thereIsAFile = false;
--- src/models/afficheModel.php
+++ src/models/afficheModel.php
@@ -26,3 +26,33 @@ function getSynopsis($id)
     unset($mySQLconnection);
     return $synopsis;
 }
+
+function getOneFilmData($id)
+{
+    $mySQLconnection = connexion();
+    $sqlQuery = 'SELECT * FROM film
+                INNER JOIN realisateur on film.id_realisateur = realisateur.id_realisateur
+                INNER JOIN personne on realisateur.id_personne = personne.id_personne
+                WHERE id_film = :id_film'; //
+    $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+    $stmt->bindValue(':id_film',$id);
+    $stmt->execute();                                                     //The data we retrieve are in array form
+    $filmData = $stmt->fetchAll();
+    unset($mySQLconnection);
+    return $filmData;
+}
+
+function getOneFilmCasting($id)
+{
+    $mySQLconnection = connexion();
+    $sqlQuery = 'SELECT * FROM film
+                INNER JOIN realisateur on film.id_realisateur = realisateur.id_realisateur
+                INNER JOIN personne on realisateur.id_personne = personne.id_personne
+                WHERE id_film = :id_film'; //
+    $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+    $stmt->bindValue(':id_film',$id);
+    $stmt->execute();                                                     //The data we retrieve are in array form
+    $casting = $stmt->fetchAll();
+    unset($mySQLconnection);
+    return $casting;
+}
