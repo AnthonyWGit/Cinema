9886ef819b094e5c4852153a836fddba9d331916
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-22 14:02:05 +0200

    new layout for all stats

---
+++ views/templates/layoutStats.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="views/templates/common.css" rel="stylesheet" />
+        <link href="views/templates/statsStyle.css" rel="stylesheet" />
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quantico:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        </style>
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="wrapper">
+                <?php require_once("header.php");?>        
+            <div class="wrapperInside">
+                <?= $content ?>
+                <?php require_once("sidebar.php")?>
+            </div>                    
+                <?php require_once("footer.php");?>
+        </div> 
+    </body>
+</html>
