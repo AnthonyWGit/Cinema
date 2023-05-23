bada7ad7914ac0f29f2e14a808b6acc66a1598b3
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-23 16:37:24 +0200

    condition 4 model having != behaviour oldID empty

--- src/controllers/filmsGenresController.php
+++ src/controllers/filmsGenresController.php
@@ -1,23 +1,26 @@
 <?php 
 require_once ('src/models/filmsGenresModel.php');
 require_once ('src/models/genreModel.php');
+require_once ('src/models/filmModel.php'); //need this one to get correct id and list of films 
 
 function displayFilmsGenres()
 {
+    $films = getFilms();
+    $genres = getgenres();
     $filmsGenres = getFilmsGenres();
     $genresFilmsList = [];
     $filmsList = [];
     //------------Building a new array of genres for dropdown-----------------
-    foreach ($filmsGenres as $filmGenre) //This will be used for the dropdown to add director when creating new row
+    foreach ($genres as $genre) //This will be used for the dropdown to add director when creating new row
     {
-        $genresFilmsList[$filmGenre["id_genre"]] = [
-            "nom_genre" => $filmGenre["nom_genre"],
-            "id" => $filmGenre["id_genre"]
+        $genresFilmsList[$genre["id_genre"]] = [
+            "nom_genre" => $genre["nom_genre"],
+            "id" => $genre["id_genre"],
         ];
     }
 
     //-------------Building a new array for film--------------
-    foreach ($filmsGenres as $film) //This will be used for the dropdown to add director when creating new row
+    foreach ($films as $film) //This will be used for the dropdown to add director when creating new row
     {
         $filmsList[$film["titre_film"]] = [
             "titre_film" => $film["titre_film"],
@@ -30,10 +33,16 @@ function displayFilmsGenres()
 
 function updateFilmGenre($id_genre,$id_film,$oldID)
 {
+    $whereIsNullActivated = false;
     $oldID = filter_var($oldID,FILTER_VALIDATE_INT);
     $id_genre_f = filter_var($id_genre,FILTER_VALIDATE_INT);
     $id_film_f = filter_var($id_film,FILTER_VALIDATE_INT);
-    updateFilmGenreModel($id_genre_f, $id_film_f,$oldID);
+    if (!$oldID) //is oldID is false it means the previous id of the genre of the film we modify is not an id or is empty so we will put a placeholder 
+                //one b/c updateFilmGenre needs need 3 valid id
+    {
+        $whereIsNullActivated = true;
+    }
+    updateFilmGenreModel($id_genre_f, $id_film_f,$oldID ,$whereIsNullActivated);
 }
 
 function addFilmGenreData($id_film,$id_genre)
