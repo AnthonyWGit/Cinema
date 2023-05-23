97dc9e4b47ac4b93bbb38249913d1c20ad3a7012
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-23 10:24:26 +0200

    controller statsActeurs need work b/c duplicates

---
+++ src/controllers/statsActeursController.php
@@ -0,0 +1,42 @@
+<?php 
+require_once ("src/models/acteurModel.php");
+require_once ("src/models/statsActeursModel.php");
+
+function displayStatsActeurs()
+{
+    $statsActeurs = getStatsActeurs();
+    $allActeurs = getActeurs();
+    $arrayFilm = [];
+    foreach ($allActeurs as $acteur) //This will be used for the dropdown to add director when creating new row
+    {
+        $acteursList[$acteur["id_acteur"]] = [
+            "name" => $acteur["nom"],
+            "forename" => $acteur["prenom"],
+            "id" => $acteur["id_acteur"]
+        ];
+    }
+    require_once("views/templates/statsActeurs.php");
+}
+
+function displayFilmActor($id)
+{
+    $arrayFilm = [];                        //Array containing values we want to display 
+    $statsActeurs = getStatsActeurs();
+    $allActeurs = getActeurs();
+    foreach ($allActeurs as $acteur) //This will be used for the dropdown to add director when creating new row
+    {
+        $acteursList[$acteur["id_acteur"]] = [
+            "name" => $acteur["nom"],
+            "forename" => $acteur["prenom"],
+            "id" => $acteur["id_acteur"]
+        ];
+    }
+
+    $acteurPlayedFilm = getStatsFilmActor($id);
+
+    foreach ($acteurPlayedFilm as $film)
+    {
+        $arrayFilm[$film["id_film"]] = ["titre_film" => $film["titre_film"]];
+    }
+    require_once("views/templates/statsActeurs.php");
+}
