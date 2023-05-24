d7459fdab611f32bf7e489f4b436b34d60b78c8d
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-24 08:52:58 +0200

    index rewritten witch switch/cases

--- index.php
+++ index.php
@@ -23,261 +23,200 @@ require_once("src/controllers/statsRolesAndCastingsController.php");
 
 //---------------------- ACTIONS UPDATE---------------------------------------
 
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateFilms")
-{   
-    $dataFilm = $_POST;
-    $idZ = $_GET["id_film"];
-    updateFilms($dataFilm, $idZ);
-}
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateActeur")
-{   
-    $dataActeurs = $_POST;
-    $id = $_GET["id_acteur"];
-    updateActeur($dataActeurs, $id);
-}
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateRealisateur")
-{   
-    $dataReals = $_POST;
-    $id = $_GET["id_real"];
-    updateReal($dataReals, $id);
-}
+if (isset($_GET["action"]))
+{
+    switch($_GET["action"])
+    {
+        case "updateFilms":
+            $dataFilm = $_POST;
+            $idZ = $_GET["id_film"];
+            updateFilms($dataFilm, $idZ);
+            break;
 
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateRole")
-{   
-    $dataRoles = $_POST;
-    $id = $_GET["id_role"];
-    updateRole($dataRoles, $id);
-}
-
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateGenre")
-{   
-    $dataGenres = $_POST;
-    $id = $_GET["id_genre"];
-    updateGenre($dataGenres, $id);
-}
-
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateCasting")
-{   
-    $dataCasting = $_POST;
-    $id = $_GET["id_film"];
-    $champ_casting= $_GET["champ_casting"];
-    updateCasting($dataCasting, $id, $champ_casting);
-}
-
-if (isset($_GET["action"]) &&  $_GET["action"] == "updateFilmGenre")
-{   
-    $oldID = $_GET["oldID"];
-    $id_genre = $_POST["id_genre"];
-    $id_film = $_GET["id"];
-    updateFilmGenre($id_genre,$id_film,$oldID);
-}
+        case "updateActeur":
+            $dataActeurs = $_POST;
+            $id = $_GET["id_acteur"];
+            updateActeur($dataActeurs, $id);
+            break;
+        case "updateRealisateur":
+            $dataReals = $_POST;
+            $id = $_GET["id_real"];
+            updateReal($dataReals, $id); 
+            break;
+        case "updateRole":
+            $dataRoles = $_POST;
+            $id = $_GET["id_role"];
+            updateRole($dataRoles, $id);   
+        case "updateGenre":
+            $dataGenres = $_POST;
+            $id = $_GET["id_genre"];
+            updateGenre($dataGenres, $id);     
+            break;
+        case "updateCasting":
+            $dataCasting = $_POST;
+            $id = $_GET["id_film"];
+            $champ_casting= $_GET["champ_casting"];
+            updateCasting($dataCasting, $id, $champ_casting); 
+            break;    
+        case "updateFilmGenre";
+            $oldID = $_GET["oldID"];
+            $id_genre = $_POST["id_genre"];
+            $id_film = $_GET["id"];
+            updateFilmGenre($id_genre,$id_film,$oldID);     
+            break; 
 //--------------------END ACTIONS UPDATE ---------------------------------------
 
-//----------------------ACTIONS DISPLAY -----------------------------------------
-
-else if (isset($_GET["action"]) && $_GET["action"] == "displayFilms")
-{
-    displayFilms();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayActeurs")
-{
-    displayActeurs();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayReals")
-{
-    displayReals();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayRoles")
-{
-    displayRoles();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayGenres")
-{
-    displayGenres();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayCastings")
-{
-    displayCastings();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayFilmsGenres")
-{
-    displayFilmsGenres();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsFilms")
-{
-    displayStatsFilms();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsReals")
-{
-    displayStatsReals();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsActeurs")
-{
-    displayStatsActeurs();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsGenres")
-{
-    displayStatsGenres();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsRoles")
-{
-    displayVoid();
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "displayStatsCastings")
-{
-    displayVoid();
-}
+//----------------------ACTIONS DISPLAY -----------------------------------------            
+        case "displayFilms":
+            displayFilms();
+            break;
+        case "displayActeurs":
+            displayActeurs();
+            break;
+        case "displayReals":
+            displayReals();
+            break;
+        case "displayRoles";
+            displayRoles();
+            break;
+        case "displayGenres":
+            displayGenres();
+            break;
+        case "displayCastings":
+            displayCastings();
+            break;
+        case "displayFilmsGenres":
+            displayFilmsGenres();
+            break;
+        case "displayStatsFilms":
+            displayStatsFilms();
+            break;
+        case "displayStatsReals":
+            displayStatsReals();
+            break;
+        case "displayStatsActeurs":
+            displayStatsActeurs();
+            break;
+        case "displayStatsGenres":
+            displayStatsGenres();
+            break;
+        case "displayStatsRoles":
+        case "displayStatsCastings":
+            displayVoid();
+            break;
 //------------------END ACTION DISPLAY-----------------------------------------------
 
 //-------------------------ACTIONS ADD STUFF ----------------------------------
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addFilm")
-{
-    $filmData = $_POST;
-    $fileData = $_FILES;
-    addFilm($filmData,$fileData);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addActeur")
-{
-    $actorData = $_POST;
-    addActeur($actorData);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addReal")
-{
-    $realData = $_POST;
-    addReal($realData);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addRole")
-{
-    $roleData = $_POST;
-    addRole($roleData);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addGenre")
-{
-    $genreData = $_POST;
-    addGenre($genreData);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "addCasting")
-{
-    $castingData = $_POST;
-    addCasting($castingData);
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "addFilmGenre")
-{
-    $id_film = $_POST["id_film"];
-    $id_genre = $_POST["id_genre"];
-    var_dump($id_film);
-    var_dump($id_genre);
-    var_dump($_POST);
-    addFilmGenreData($id_film,$id_genre);
-}
+        case "addFilm":
+            $filmData = $_POST;
+            $fileData = $_FILES;
+            addFilm($filmData,$fileData);
+            break;
+        case "addActeur":
+            $actorData = $_POST;
+            addActeur($actorData);        
+            break;
+        case "addReal":
+            $realData = $_POST;
+            addReal($realData);
+            break;
+        case "addRole":
+            $roleData = $_POST;
+            addRole($roleData);       
+            break;
+        case "addGenre":
+            $genreData = $_POST;
+            addGenre($genreData);
+            break;
+        case "addCasting":
+            $castingData = $_POST;
+            addCasting($castingData);
+            break;
+        case "addFilmGenre":
+            $id_film = $_POST["id_film"];
+            $id_genre = $_POST["id_genre"];
+            var_dump($id_film);
+            var_dump($id_genre);
+            var_dump($_POST);
+            addFilmGenreData($id_film,$id_genre);
+            break;
 //----------------------------------END ACTIONS ADD STUFF ----------------------------
 
 //------------------------------ACTION DELETE STUFF-------------------------------------
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteFilm")
-{
-    $id = $_GET["id_film"];
-    deleteFilm($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteActeur")
-{
-    $id = $_GET["id_acteur"];
-    deleteActeur($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteReal")
-{
-    $id = $_GET["id_real"];
-    deleteReal($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteRole")
-{
-    $id = $_GET["id_role"];
-    deleteRole($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteGenre")
-{
-    $id = $_GET["id_genre"];
-    deleteGenre($id);
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteCasting")
-{
-    $id_film = $_GET["id_film"];
-    $id_acteur = $_GET["id_acteur"];
-    $id_role = $_GET["id_role"];
-    deleteCasting($id_film, $id_acteur, $id_role);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "deleteFilmGenre")
-{
-    $id_film = $_GET["id_film"];
-    $id_genre = $_GET["id_genre"];
-    deleteFilmGenre($id_film, $id_genre);
-}
+        case "deleteFilm":
+            $id = $_GET["id_film"];
+            deleteFilm($id);      
+            break;
+            
+        case "deleteActeur":
+            $id = $_GET["id_acteur"];
+            deleteActeur($id); 
+            break;
+        case "deleteReal":
+            $id = $_GET["id_real"];
+            deleteReal($id);    
+            break;
+        case "deleteRole":
+            $id = $_GET["id_role"];
+            deleteRole($id);
+            break;
+        case "deleteGenre":
+            $id = $_GET["id_genre"];
+            deleteGenre($id);        
+            break;    
+        case "deleteCasting":
+            $id_film = $_GET["id_film"];
+            $id_acteur = $_GET["id_acteur"];
+            $id_role = $_GET["id_role"];
+            deleteCasting($id_film, $id_acteur, $id_role);
+            break;
+        case "deleteFilmGenre":
+            $id_film = $_GET["id_film"];
+            $id_genre = $_GET["id_genre"];
+            deleteFilmGenre($id_film, $id_genre);       
+            break;     
 // ----------------------------END ACTION DELETE STUFF----------------------------------------
 
 //----------------------------ACTION UPLOAD FILE-----------------------------------------
-
-else if (isset($_GET["action"]) && $_GET["action"] == "uploadFile")
-{
-    $id = $_GET["id_film"];
-    $file = $_FILES;
-    var_dump($_FILES);
-    uploadFile($file,$id);
-}
+        case "uploadFile":
+            $id = $_GET["id_film"];
+            $file = $_FILES;
+            var_dump($_FILES);
+            uploadFile($file,$id);   
+            break;
 
 //-----------------------------------END ACTION UPLOAD FILE ------------------------------------
 
 //-------------------------------MISC--------------------------------------------------
 
-else if (isset($_GET["action"]) && $_GET["action"] == "goToSynopsis")
-{
-    $id = $_GET["id"];
-    displaySynopsis($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "editSynopsis")
-{
-    $id = $_GET["id"];
-    $textSynopsis = $_POST["textSynopsis"];
-    editSynopsis($textSynopsis, $id);
-}
-else if (isset($_GET["action"]) && $_GET["action"] == "goToAffiche")
-{
-    $id = $_GET["id"];
-    displayAffiche($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "getRealsStats")
-{
-    $id = $_POST["id_realisateur"];
-    displayStatsOneReal($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "getRealsStatsActorCheck")
-{
-    $id = $_POST["id_realisateur"];
-    displayStatsOneRealIsActor($id);
-}
-
-else if (isset($_GET["action"]) && $_GET["action"] == "getActorFilm")           //USED TO DISPLAY WICH ACTOR HAS PLAYED IN WICH FILM 
-{
-    $id = $_POST["id_acteur"];
-    displayFilmActor($id);
+        case "goToSynopsis":
+            $id = $_GET["id"];
+            displaySynopsis($id);  
+            break;
+        case "editSynopsis":
+            $id = $_GET["id"];
+            $textSynopsis = $_POST["textSynopsis"];
+            editSynopsis($textSynopsis, $id);     
+            break;
+        case "goToAffiche":
+            $id = $_GET["id"];
+            displayAffiche($id);    
+        case "getRealsStats":
+            $id = $_POST["id_realisateur"];
+            displayStatsOneReal($id);        
+            break;
+        case "getRealsStatsActorCheck":
+            $id = $_POST["id_realisateur"];
+            displayStatsOneRealIsActor($id);
+            break;
+        case "getActorFilm"://USED TO DISPLAY WICH ACTOR HAS PLAYED IN WICH FILM 
+            $id = $_POST["id_acteur"];
+            displayFilmActor($id); 
+            break;                                    
+    }
 }
 // ---------------------------------DEFAULT : HOMEPAGE--------------------------------
-
 else //page when landing on site 
 {
     landingOnWebsite();
 }
-
