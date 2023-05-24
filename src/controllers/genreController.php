4d1e93033c99a19be025ca76bc3b805c22f852c6
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-24 16:57:08 +0200

    renamed cinemaController into genreController

--- src/controllers/genreController.php
+++ src/controllers/genreController.php
@@ -1,28 +1,73 @@
-<?php 
-require_once ('src/models/genreModel.php');
+<?php
 
-function displayGenres()
+namespace Controllers;
+use Models\Connect;
+
+class GenreController
 {
-    $genres = getGenres();
-    require "views/templates/genreListing.php";
-}
+    public function displayGenres()
+    {
+        //----------SQL PART-----------------------
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM genre'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);
+        $stmt->execute();
+        $genres = $stmt->fetchAll();
+        $genres = getGenres();
+        //-----------------------------------------
 
-function updateGenre($dataGenres, $id)
+        require "views/templates/genreListing.php";
+        return $genres;        
+    }
+    
+    function addGenre($genreData)
+    {
+        $filteredGenreData = filter_var($genreData["nom_genre"],FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        //---------------------SQL PART-------------------------------
+        $mySQLconnection = connexion();
+        $sqlQuery = 'INSERT INTO genre (nom_genre) VALUES (:nom_genre)';
+        $stmt = $mySQLconnection->prepare($sqlQuery);
+        $stmt->bindValue(':nom_genre',$filteredGenreData);
+        $stmt->execute();
+        //-------------------------------------------------------------
+    }
+
+    function deleteGenre($id)
+    {
+    //------------------------------SQL PART
+           // We need to get rid of the entries in genrer table where there is the id of genre we want to dolete 
+    $mySQLconnection = connexion();
+    $sqlQuery = 'DELETE FROM genrer
+                WHERE id_genre = :id_genre';
+    $stmt = $mySQLconnection->prepare($sqlQuery);
+    $stmt->bindValue(':id_genre',$id, \PDO::PARAM_INT);
+    $stmt->execute();
+
+
+    $sqlQuery = 'DELETE FROM genre
+                WHERE id_genre = :id_genre';
+    $stmt = $mySQLconnection->prepare($sqlQuery);
+    $stmt->bindValue(':id_genre',$id, \PDO::PARAM_INT);
+    $stmt->execute();
+    //-------------------------------------------
+    }
+
+    function updateGenre($dataGenres, $id)
 {
     foreach ($dataGenres as $fieldName=>$value)
     {
         $filteredValue = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);    //Sanitizing value in array
         $dataGenre[$fieldName] = $filteredValue;                                     //replacing original values by sanitized
     }
-    updateGenreModel($filteredValue,$id);
+//--------------SQL PART------------------------------------
+    $mySQLconnection = connexion();
+    $sqlQuery = 'UPDATE genre SET nom_genre = :nom_genre
+                WHERE id_genre = :id_genre';
+    $stmt =  $mySQLconnection->prepare($sqlQuery);
+    $stmt->bindValue(':nom_genre',$filteredValue);
+    $stmt->bindValue(':id_genre',$id,\PDO::PARAM_INT);
+    $stmt->execute();
+//----------------------------------------------------------
 }
-
-function addGenre($genreData)
-{
-    $filteredGenreData = filter_var($genreData["nom_genre"],FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    addGenreModel($filteredGenreData);
-}
-function deleteGenre($id)
-{
-    deleteGenreModel($id);
 }
