3a51689d8ac4e76c47c6faf362039c8c69a492c2
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-25 16:29:22 +0200

    file rewritten with oop approach

--- src/controllers/statsFilmsController.php
+++ src/controllers/statsFilmsController.php
@@ -1,10 +1,41 @@
 <?php 
-require_once ("src/models/filmModel.php");
-require_once ("src/models/statsFilmsModel.php");
+// require_once ("src/models/filmModel.php");
+// require_once ("src/models/statsFilmsModel.php");
 
-function displayStatsFilms()
+namespace Controllers;
+use Models\Connect;
+
+class statsFilmsController
 {
-    $filmsFiveYears = getStatsCinqFiveYears();
-    $filmsLenght = getStatsDeuxHeures();
-    require ("views/templates/statsFilms.php");
+    function getFiveYears()
+    {
+        $mySQLconnexion = Connect::connexion();
+        $sql = 'SELECT film.titre_film  FROM film
+                WHERE DATE_FORMAT( CURDATE() , "%Y") - CONVERT(film.dateSortie_film, CHAR) < 5';
+        $stmt = $mySQLconnexion->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        return $data;
+    }
+
+    function getTwoHours()
+    {
+        $mySQLconnexion = Connect::connexion();
+        $sql = 'SELECT film.titre_film, DATE_FORMAT(SEC_TO_TIME(film.duree_film * 60), "%H:%i") AS "Durée" FROM film 
+                WHERE film.duree_film > 135
+                ORDER BY film.duree_film DESC';
+        $stmt = $mySQLconnexion->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        return $data;
+    }
+
+    function displayStatsFilms()
+    {
+        $filmsFiveYears = $this->getFiveYears();
+        $filmsLenght = $this->getTwoHours();
+        require ("views/templates/statsFilms.php");
+    }
+
 }
+
