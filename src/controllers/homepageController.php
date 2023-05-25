f41ff8f895d596295371df7826046437a8277ce9
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-25 13:11:23 +0200

    writing controller for landing on website

--- src/controllers/homepageController.php
+++ src/controllers/homepageController.php
@@ -1,9 +1,30 @@
 <?php 
-require_once "src/models/homepageModel.php";
+//require_once "src/models/homepageModel.php";
 
-function landingOnWebsite()
-{   //The fetLastUpdate function is borked. It uses information.schema_tables and this doesn't count changes done via web forms and stuff
-    $lastUpdatedItems = getLastUpdate();
-    require("views/templates/homepage.php") ;
+namespace Controllers;
+use Models\Connect;
+
+class HomepageController
+{
+    function landingOnWebsite()
+    {   //The fetLastUpdate function is borked. It uses information.schema_tables and this doesn't count changes done via web forms and stuff
+
+        //------------------SQL REQUEST------------------------------------
+        $mySQLconnexion = Connect::connexion();
+        $sqlWHEREpart = "";
+        $sql = 'SELECT 
+        TABLE_NAME AS TableName, 
+        update_time AS LastUpdated 
+    FROM 
+        information_schema.tables 
+    WHERE 
+        table_schema = "cinema" ';
+        $stmt = $mySQLconnexion->prepare($sql);
+        $stmt->execute();
+        $dateTime = $stmt->fetchAll();
+
+
+        require("views/templates/homepage.php") ;
+        //--------------------------END SQL-------------------------------------
+    }
 }
-
