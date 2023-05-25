4345d5590d43be94d73568539ced4ade53d18375
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-25 13:11:03 +0200

    class math created

--- src/controllers/math.php
+++ src/controllers/math.php
@@ -1,20 +1,27 @@
 <?php   /* This file is used in cases we need to do arithmetics*/
-function filterFourNumbers($filteredValue)
-{
-    echo "String matches pattern!";
-    $hours = substr($filteredValue, 0, 2); // extract the first two characters (hours)
-    $minuts = substr($filteredValue, 3, 2); // same but lasts
-    $totalMinutes = ($hours * 60) + $minuts; // calculate the total number of minuts
-    $filteredValue = $totalMinutes;
-    return $filteredValue;
-}
 
-function filterThreeNumbers($filteredValue)
+namespace Controllers;
+
+
+class Math
 {
-    echo "String matches pattern!";
-    $hours = substr($filteredValue, 0, 1); // extract the first character (hours)
-    $minuts = substr($filteredValue, 2, 2); // same but lasts
-    $totalMinutes = ($hours * 60) + $minuts; // calculate the total number of minuts
-    $filteredValue = $totalMinutes;
-    return $filteredValue;
+    static function filterFourNumbers($filteredValue)
+    {
+        echo "String matches pattern!";
+        $hours = substr($filteredValue, 0, 2); // extract the first two characters (hours)
+        $minuts = substr($filteredValue, 3, 2); // same but lasts
+        $totalMinutes = ($hours * 60) + $minuts; // calculate the total number of minuts
+        $filteredValue = $totalMinutes;
+        return $filteredValue;
+    }
+
+    static function filterThreeNumbers($filteredValue)
+    {
+        echo "String matches pattern!";
+        $hours = substr($filteredValue, 0, 1); // extract the first character (hours)
+        $minuts = substr($filteredValue, 2, 2); // same but lasts
+        $totalMinutes = ($hours * 60) + $minuts; // calculate the total number of minuts
+        $filteredValue = $totalMinutes;
+        return $filteredValue;
+    }    
 }
