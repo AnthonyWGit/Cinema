e33fe0415f33ca153b69da509331c535c757159e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-05-31 10:49:50 +0200

    header same as headervisitor

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,8 +3,18 @@
         <div class="logo">
             <h2>LOGO</h2>
         </div>
-        <p><a href="index.php?action=displayVisitorFilm">Version visiteur</a></p>
-        <button id="mode-select" class="toggleBtn" onclick='myFunction()'><i class="bi bi-sun" id="sun-moon"></i></button>
+        <ul>
+            <li><a href="index.php?action=displayVisitorFilm">Menu</a></li>
+            <li><a href="index.php?action=Homepage">Admin interface</a></li>
+        </ul>
+            <button id="mode-select" class="toggleBtn" onclick='myFunction()'><i class="bi bi-sun" id="sun-moon"></i></button>
+            <div class="div">
+                <?= (!isset($_SESSION["session"])) ? '<h5><a href="index.php?action=goToLogin">Login</a></h5>' : '' ?>
+            </div>
+            <div class="div">
+                <h5><a href="index.php?action=goToRegister">S'inscrire</a></h5>
+            </div>
+                <?= (isset($_SESSION["session"]) && $_SESSION["session"]) ? "<h5><a href='index.php?action=disconnect'>Déconnexion</a></h5>" : "" ?>            
         <div class="lang">
             <h2>FR</h2>
         </div>
