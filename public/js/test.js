3e755f58760732e8a3e987252039b2087875c952
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-01 16:52:09 +0200

    user js on: set email text. controller will check

--- public/js/test.js
+++ public/js/test.js
@@ -9,15 +9,39 @@ document.addEventListener("DOMContentLoaded", function() { //waiting for all con
     return true;
   }
 
-  function checkInputsAndToggleModal() {
+  // function isEmailValid(email)
+  // {
+  //   // Regular expression pattern for email validation
+  //   var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //   return emailPattern.test(email);
+  // }
+
+  // function checkEmail() 
+  // {
+  //   var email = emailInput.value.trim();
+  //   if (!isEmailValid(email)) 
+  //   {
+  //     modalButton.removeAttribute("data-bs-toggle");
+  //     modalButton.removeAttribute("data-bs-target");
+  //   }
+  // }
+
+  function checkInputsAndToggleModal() 
+  {
     if (!checkFormInputsNotEmpty()) {
+      form.action = "index.php?action=checkInfosModalVide";
+      iframe.src = "index.php?action=checkInfosModalVide";
       // alert("Please fill in all form fields.");
     } else {
-      iframe.src = "index.php?action=checkInfos"; // Set the URL here
+      form.action = "index.php?action=checkInfosModal";
+      iframe.src = "index.php?action=checkInfosModal"; // Set the URL here
+      
     }
   }
 
 ;
+
+  var emailInput = document.getElementById("email-input");
   const iframe = document.getElementById("iframe");
   const form = document.querySelector("#form-register");
   const modalButton = document.getElementById("password-input-confirm-modal");
@@ -25,6 +49,9 @@ document.addEventListener("DOMContentLoaded", function() { //waiting for all con
 
   form.target = "iframe"
   modalButton.addEventListener("click", checkInputsAndToggleModal);
+  // modalButton.addEventListener("click", checkEmail);
+  form.action = "index.php?action=checkInfosModal";
+  emailInput.type = "text"
 
   inputs.forEach(function(input) {
     input.removeAttribute("required");
